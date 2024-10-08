774e545149872700d6f5215b7db8a6653b246a8c
baluthripurari <baluthripurari@gmail.com>
2024-10-08 21:33:26 +0530

    creating security groups

--- 02-security-group(sg)/main.tf
+++ 02-security-group(sg)/main.tf
@@ -31,3 +31,28 @@ module "frontend" {
   common_tags = var.common_tags
   sg_name = "frontend"
 }
+
+module "bastion" {
+  source = "../../terraform-aws-securitygroup"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for Bastion Instances"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "bastion"
+}
+
+module "ansible" {
+  source = "../../terraform-aws-securitygroup"
+  project_name = var.project_name
+  environment = var.environment
+  sg_description = "SG for ansible Instance"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  common_tags = var.common_tags
+  sg_name = "ansible"
+}
+
+
+resource "aws_security_grope.0" "name" {
+  
+}
